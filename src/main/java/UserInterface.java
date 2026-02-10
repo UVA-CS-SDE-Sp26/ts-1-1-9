fa6c393f2956c628c2d42475c7fd6645893faec3
Styx <styx@styxd.dev>
2026-02-10 11:49:33 -0500

    Begin implementing parseArguments

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -2,6 +2,32 @@ public class UserInterface {
 
     public UserInterface() {}
 
-    public void parseArguments(String[] arguments) {
+    public String parseArguments(String[] arguments) throws IllegalArgumentException {
+        // we can accept a maximum of 2 arguments
+        if (arguments.length > 2) {
+            throw new IllegalArgumentException("Too many arguments");
+        }
+        // no arguments means we print out a file list
+        if (arguments.length == 0) {
+            String fileList = "";
+            // stub: ask ProgramControl for file list
+            return fileList;
+        }
+
+        String cipherFile = "key.txt";
+        // if we received two arguments, we need to use the second as the cipherFile instead of the default
+        if (arguments.length == 2) {
+            // verify that this is actually a file?
+            cipherFile = arguments[1];
+        }
+        // try to get the file contents
+        try {
+            int fileNumber = Integer.parseInt(arguments[0]);
+            String fileContents = "";
+            // stub: call ProgramControl with fileNumber and cypherFile to get file contents
+            return fileContents;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("First argument should be a number");
+        }
     }
 }
--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -4,20 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserInterfaceTest {
 
+    String testFileList = "1 file1\n2 file 2\n3 file 3";
+    String testFileContents = "contents of a file";
+
     @Test
     public void parseArgumentsZero() {
         UserInterface ui = new UserInterface();
         String[] arguments = new String[]{};
-        ui.parseArguments(arguments);
-        // expect file list
+        String result = ui.parseArguments(arguments);
+        assertEquals(testFileList, result);
     }
 
     @Test
     public void parseArgumentsOneValid() {
         UserInterface ui = new UserInterface();
         String[] arguments = new String[]{"1"};
-        ui.parseArguments(arguments);
-        // expect file contents using the default key
+        String result = ui.parseArguments(arguments);
+        assertEquals(testFileContents, result);
     }
 
     @Test
@@ -31,7 +34,8 @@ class UserInterfaceTest {
     public void parseArgumentsTwoValid() {
         UserInterface ui = new UserInterface();
         String[] arguments = new String[]{"1", "key.txt"};
-        // expect deciphered file contents using the key
+        String result = ui.parseArguments(arguments);
+        assertEquals(testFileContents, result);
     }
 
     @Test
@@ -39,8 +43,8 @@ class UserInterfaceTest {
         UserInterface ui = new UserInterface();
         String[] arguments_first = new String[]{"invalid", "key.txt"};
         assertThrows(IllegalArgumentException.class, () -> ui.parseArguments(arguments_first), "Should throw IllegalArgumentException as the first argument is not a number");
-        String[] arguments_second = new String[]{"1", "not a cipher"};
-        assertThrows(IllegalArgumentException.class, () -> ui.parseArguments(arguments_second), "Should throw IllegalArgumentException as the second argument is not a cypher");
+        //String[] arguments_second = new String[]{"1", "not a cipher"};
+        //assertThrows(IllegalArgumentException.class, () -> ui.parseArguments(arguments_second), "Should throw IllegalArgumentException as the second argument is not a cypher");
     }
 
     @Test
